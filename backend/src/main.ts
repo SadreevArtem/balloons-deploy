c4d1be78821123de50d060928c1a019e42bdaf52
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-08-21 18:48:15 +0500

    cors option

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT || 4000);
