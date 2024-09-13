a15c451b255020b068902cb84bec29b070090303
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-09-13 11:18:46 +0500

    seo

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import "@/styles/globals.css";
 import { HydrationBoundary,QueryClientProvider,QueryClient } from "@tanstack/react-query";
 import { NextPage } from "next";
@@ -18,10 +19,39 @@ export default function App({ Component, pageProps }: AppProps) {
     },
   }))
   return getLayout(
+    <>
+    <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+       
+          ym(98326099, "init", {
+               clickmap:true,
+               trackLinks:true,
+               accurateTrackBounce:true,
+               webvisor:true
+          });
+              `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/98326099"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
     <QueryClientProvider client={queryClient}>
        <HydrationBoundary state={pageProps.dehydratedState}>
       <Component {...pageProps} />
       </HydrationBoundary>
     </QueryClientProvider>
+    </>
   );
 }
