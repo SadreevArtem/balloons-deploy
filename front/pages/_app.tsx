c5258eef4d4c0ff34d9597f750823f90960fa002
123 <a123@123s-MacBook-Air.local>
2024-11-19 18:50:39 +0500

    fix banner

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,43 +1,50 @@
 /* eslint-disable @next/next/no-img-element */
 import "@/styles/globals.css";
-import { HydrationBoundary,QueryClientProvider,QueryClient } from "@tanstack/react-query";
+import {
+  HydrationBoundary,
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { ReactElement, ReactNode, useState } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = (Component as NextPageWithLayout).getLayout ?? ((page: ReactElement) => page)
-  const [queryClient] = useState(()=> new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  }))
+  const getLayout =
+    (Component as NextPageWithLayout).getLayout ??
+    ((page: ReactElement) => page);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return getLayout(
     <>
-    <script
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      <Script id="metrika-counter" strategy="afterInteractive">
+        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-       
+ 
           ym(98326099, "init", {
-               clickmap:true,
-               trackLinks:true,
-               accurateTrackBounce:true,
-               webvisor:true
-          });
-              `,
-        }}
-      />
+                defer: true,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+          });`}
+      </Script>
       <noscript>
         <div>
           <img
@@ -47,11 +54,11 @@ export default function App({ Component, pageProps }: AppProps) {
           />
         </div>
       </noscript>
-    <QueryClientProvider client={queryClient}>
-       <HydrationBoundary state={pageProps.dehydratedState}>
-      <Component {...pageProps} />
-      </HydrationBoundary>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </HydrationBoundary>
+      </QueryClientProvider>
     </>
   );
 }
--- front/shared/components/Author/Author.tsx
+++ front/shared/components/Author/Author.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 import { FC, useCallback, useEffect, useRef, useState } from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 
-
 type BannerProps = {
-  images: {url: string}[];
+  images: { url: string }[];
 };
 
 export const Author: FC<BannerProps> = ({ images }) => {
-
   const [activeBannerIndex, setActiveBannerIndex] = useState(0);
   const activeBanner = images[activeBannerIndex];
   const imageRef = useRef<HTMLImageElement>(null);
 
   const onNextBannerIndex = useCallback(() => {
-    setActiveBannerIndex((prevState: number) => (prevState === images.length - 1 ? 0 : prevState + 1));
+    setActiveBannerIndex((prevState: number) =>
+      prevState === images.length - 1 ? 0 : prevState + 1
+    );
   }, [setActiveBannerIndex, images]);
 
   useEffect(() => {
@@ -26,7 +26,7 @@ export const Author: FC<BannerProps> = ({ images }) => {
   return (
     <div className="relative w-full">
       <div className="flex justify-center items-start">
-        <div className="h-[60vh]">
+        <div className="h-[60vh] max-md:hidden">
           <SwitchTransition mode="in-out">
             <CSSTransition
               nodeRef={imageRef}
@@ -52,8 +52,23 @@ export const Author: FC<BannerProps> = ({ images }) => {
             </CSSTransition>
           </SwitchTransition>
         </div>
+        <div className="md:hidden h-[60vh] relative w-full">
+          <Image
+            src={"/images/banner3mob.jpg"}
+            alt="Banner"
+            width={768}
+            height={512}
+            priority
+            sizes="100vw"
+          />
+        </div>
       </div>
-      <Link className="absolute bottom-[36px] md:bottom-[72px] bg-white opacity-75 rounded-[80px] text-primary md:text-2xl text-lg md:px-16 px-8 md:py-3 py-2 left-1/2 transform -translate-x-1/2" href={"/catalog"}>Каталог</Link>
+      <Link
+        className="absolute bottom-[36px] md:bottom-[72px] bg-white opacity-75 rounded-[80px] text-primary md:text-2xl text-lg md:px-16 px-8 md:py-3 py-2 left-1/2 transform -translate-x-1/2"
+        href={"/catalog"}
+      >
+        Каталог
+      </Link>
     </div>
   );
 };
