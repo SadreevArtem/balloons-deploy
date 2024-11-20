9344cdaf205f1c487c7535cf3e338d64bfb113fc
123 <a123@123s-MacBook-Air.local>
2024-11-20 22:31:40 +0500

    fix metrica

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -7,7 +7,6 @@ import {
 } from "@tanstack/react-query";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 import { ReactElement, ReactNode, useState } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -30,21 +29,24 @@ export default function App({ Component, pageProps }: AppProps) {
   );
   return getLayout(
     <>
-      <Script id="metrika-counter" strategy="afterInteractive">
-        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
+       
           ym(98326099, "init", {
-                defer: true,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-          });`}
-      </Script>
+               clickmap:true,
+               trackLinks:true,
+               accurateTrackBounce:true,
+               webvisor:true
+          });
+              `,
+        }}
+      />
       <noscript>
         <div>
           <img
